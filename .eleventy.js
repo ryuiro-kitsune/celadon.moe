512d8052ba7727e605564546802372b21ccbb9d4
Zane <zane@beckerbyrd.com>
2022-11-27 19:32:05 -0800

    Boilerplate

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addCollection("posts", function(collection) {
+        return collection.getFilteredByGlob("posts/*.md");
 }
