88442f896787e1cb302292deb48d6a4c7564d81f
Zane <zane@beckerbyrd.com>
2022-12-01 11:14:10 -0600

    Markdown experiement

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
 module.exports = function(eleventyConfig) {
+    // Markdown experiement
+    let markdownIt = require("markdown-it");
+    let markdownItFootnote = require("markdown-it-footnote");
+    
+    let options = {
+	html: true, // Enable HTML tags in source
+	breaks: true,  // Convert '\n' in paragraphs into <br>
+	linkify: true // Autoconvert URL-like text to links
+    };
+    
+    // configure the library with options
+    let markdownLib =  markdownIt(options).use(markdownItFootnote);
+    // set the library to process markdown files
+    config.setLibrary("md", markdownLib);
+    
+    
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addCollection("posts", function(collection) {
