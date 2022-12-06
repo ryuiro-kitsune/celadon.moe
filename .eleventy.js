237da7c76f2a9dd03b2a1d05712a0db69d708d70
Zane <zane@beckerbyrd.com>
2022-12-05 20:16:22 -0600

    Fix css path

--- .eleventy.js
+++ .eleventy.js
@@ -18,8 +18,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setLibrary("md", markdownLib);
     
     eleventyConfig.addPassthroughCopy("assets");
+<<<<<<< Updated upstream
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("*.html");
+=======
+    eleventyConfig.addPassthroughCopy("source/css/");
+	eleventyConfig.addPassthroughCopy("*.html");
+>>>>>>> Stashed changes
     eleventyConfig.addCollection("posts", function(collection) {
         return collection.getFilteredByGlob("posts/*.md");
     });
