c5658dff6554820425265e5b8b86e0fcd8449cda
Zane Becker-Byrd <zane@beckerbyrd.com>
2024-12-14 20:55:33 -0800

    sass support

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,20 @@
 //Imports
+import sass from "sass";
 
 //Config
-export default async function(eleventyConfig) {};
+export default async function(eleventyConfig) {
+	// Add Sass Scss plugin and configure
+	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		compile: async function (inputContent) {
+			let result = sass.compileString(inputContent);
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
+};
 
 
 //Directories
